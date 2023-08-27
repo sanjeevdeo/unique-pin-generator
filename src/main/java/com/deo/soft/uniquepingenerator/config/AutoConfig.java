82c74b01aa2af6da5c4d3a6e0aba7beb37ca68df
sanjeevdeo <get.sanjeevdeo@gmail.com>
2023-08-27 23:45:07 +1200

    feat: Enable auto configuration

---
+++ src/main/java/com/deo/soft/uniquepingenerator/config/AutoConfig.java
@@ -0,0 +1,10 @@
+package com.deo.soft.uniquepingenerator.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan("com.deo.soft.uniquepingenerator")
+public class AutoConfig {
+
+}
